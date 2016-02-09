1d7aed7f0003fdf3438e1ee9f5fc90019521513a
Alex Miller <amiller@hortonworks.com>
2016-02-08 19:45:19 -0500

    initial attempt at installation script

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo 'Installing nodejs, npm, & nvm'
+rundir=`pwd`
+export HDPCLUSTER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Installing nvm & npm from Homebrew..."
 brew install -g nvm npm
+echo "Installing nodejs 0.12 using nvm..."
 nvm install 0.12
-
 echo ''
-cd http-server
+
+echo "Installing http-server using npm..."
+cd ${HDPCLUSTER_HOME}/http-server
 npm install
-
 echo ''
-cd ..
-./webapp.sh
+
+echo "Appending alias and ENV vars to ~/.bash_profile"
+echo -e "\n# hdpcluster utility" >> ~/.bash_profile
+echo "export HDPCLUSTER_HOME=${HDPCLUSTER_HOME}" >> ~/.bash_profile
+echo "alias hdpcluster='${HDPCLUSTER_HOME}/hdpcluster.sh'" >> ~/.bash_profile
+
+echo "Setup complete!"
+cd $rundir
